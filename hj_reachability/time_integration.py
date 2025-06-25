88f8afc5759f66b58e0a915bb26fa3efc7ba10ea
Bartosz Jemioł <80482453+Extrabartek@users.noreply.github.com>
2025-06-25 16:20:19 +0200

    Update time_integration.py
    Adding CBVF time integrator

--- hj_reachability/time_integration.py
+++ hj_reachability/time_integration.py
@@ -15,7 +15,7 @@ def lax_friedrichs_numerical_hamiltonian(hamiltonian, state, time, value, left_g
 
 
 @functools.partial(jax.jit, static_argnames="dynamics")
-def euler_step(solver_settings, dynamics, grid, time, values, time_step=None, max_time_step=None):
+def euler_step(solver_settings, dynamics, grid, time, values, time_step=None, max_time_step=None, time_step_bound=0.0001):
     time_direction = jnp.sign(max_time_step) if time_step is None else jnp.sign(time_step)
     signed_hamiltonian = lambda *args, **kwargs: time_direction * dynamics.hamiltonian(*args, **kwargs)
     left_grad_values, right_grad_values = grid.upwind_grad_values(solver_settings.upwind_scheme, values)
@@ -28,7 +28,32 @@ def euler_step(solver_settings, dynamics, grid, time, values, time_step=None, ma
                                               left_grad_value, right_grad_value, dissipation_coefficients)),
         np.arange(grid.ndim))(grid.states, values, left_grad_values, right_grad_values, dissipation_coefficients))
     if time_step is None:
-        time_step_bound = 1 / jnp.max(jnp.sum(dissipation_coefficients / jnp.array(grid.spacings), -1))
+        if time_step_bound is None:
+            time_step_bound = 1 / jnp.max(jnp.sum(dissipation_coefficients / jnp.array(grid.spacings), -1))
+        time_step = time_direction * jnp.minimum(solver_settings.CFL_number * time_step_bound, jnp.abs(max_time_step))
+    # TODO: Think carefully about whether `solver_settings.value_postprocessor` should be applied here instead.
+    return time + time_step, values + time_step * dvalues_dt
+
+@functools.partial(jax.jit, static_argnames="dynamics")
+def euler_step_cbvf(solver_settings, dynamics, grid, time, values, time_step=None, max_time_step=None, time_step_bound=0.0001):
+    time_direction = jnp.sign(max_time_step) if time_step is None else jnp.sign(time_step)
+    signed_hamiltonian = lambda *args, **kwargs: time_direction * dynamics.hamiltonian(*args, **kwargs)
+    left_grad_values, right_grad_values = grid.upwind_grad_values(solver_settings.upwind_scheme, values)
+    dissipation_coefficients = solver_settings.artificial_dissipation_scheme(dynamics.partial_max_magnitudes,
+                                                                             grid.states, time, values,
+                                                                             left_grad_values, right_grad_values)
+
+    hamiltonian_values = utils.multivmap(
+        lambda state, value, left_grad_value, right_grad_value, dissipation_coefficients:
+        (lax_friedrichs_numerical_hamiltonian(signed_hamiltonian, state, time, value,
+                                              left_grad_value, right_grad_value, dissipation_coefficients)),
+        np.arange(grid.ndim))(grid.states, values, left_grad_values, right_grad_values, dissipation_coefficients)
+
+    dvalues_dt = -solver_settings.hamiltonian_postprocessor(time_direction * (hamiltonian_values
+                                                                              + solver_settings.gamma * values))
+    if time_step is None:
+        if time_step_bound is None:
+            time_step_bound = 1 / jnp.max(jnp.sum(dissipation_coefficients / jnp.array(grid.spacings), -1))
         time_step = time_direction * jnp.minimum(solver_settings.CFL_number * time_step_bound, jnp.abs(max_time_step))
     # TODO: Think carefully about whether `solver_settings.value_postprocessor` should be applied here instead.
     return time + time_step, values + time_step * dvalues_dt
@@ -52,4 +77,12 @@ def third_order_total_variation_diminishing_runge_kutta(solver_settings, dynamic
     _, values_2 = euler_step(solver_settings, dynamics, grid, time_1, values_1, time_step)
     time_0_5, values_0_5 = time + time_step / 2, (3 / 4) * values + (1 / 4) * values_2
     _, values_1_5 = euler_step(solver_settings, dynamics, grid, time_0_5, values_0_5, time_step)
+    return time_1, solver_settings.value_postprocessor(time_1, (1 / 3) * values + (2 / 3) * values_1_5)
+
+def third_order_total_variation_diminishing_runge_kutta_cbvf(solver_settings, dynamics, grid, time, values, target_time):
+    time_1, values_1 = euler_step_cbvf(solver_settings, dynamics, grid, time, values, max_time_step=target_time - time)
+    time_step = time_1 - time
+    _, values_2 = euler_step_cbvf(solver_settings, dynamics, grid, time_1, values_1, time_step)
+    time_0_5, values_0_5 = time + time_step / 2, (3 / 4) * values + (1 / 4) * values_2
+    _, values_1_5 = euler_step_cbvf(solver_settings, dynamics, grid, time_0_5, values_0_5, time_step)
     return time_1, solver_settings.value_postprocessor(time_1, (1 / 3) * values + (2 / 3) * values_1_5)
