d75a414805944a6ae4d33f67284fb95a0a8b7026
Bartosz Jemioł <b.s.jemiol@student.tudelft.nl>
2025-07-01 11:30:36 +0200

    exposed the new solver to the rest of the package

--- cbvf_reachability/__init__.py
+++ cbvf_reachability/__init__.py
@@ -8,10 +8,10 @@ from cbvf_reachability import time_integration
 from cbvf_reachability import utils
 from cbvf_reachability.dynamics import ControlAndDisturbanceAffineDynamics, Dynamics
 from cbvf_reachability.grid import Grid
-from cbvf_reachability.solver import SolverSettings, solve, step
+from cbvf_reachability.solver import SolverSettings, solve, step, step_vi, step_cbvf, solve_vi, solve_cbvf
 
 __version__ = "0.7.0"
 
 __all__ = ("ControlAndDisturbanceAffineDynamics", "Dynamics", "Grid", "SolverSettings", "artificial_dissipation",
            "boundary_conditions", "finite_differences", "sets", "solve", "solver", "step", "systems",
-           "time_integration", "utils")
+           "step_vi", "step_cbvf", "solve_vi", "solve_cbvf", "time_integration", "utils")
